a8f13ab14133d0aa2257ff968367e937041eaa07
mat3625 <mat3625@g.rit.edu>
2014-02-03 22:52:03 -0500

    Added Right Click context menu to the Data Editor

--- src/LinGUIne/parts/DataEditorPart.java
+++ src/LinGUIne/parts/DataEditorPart.java
@@ -10,36 +10,118 @@ import org.eclipse.swt.SWT;
 import org.eclipse.swt.custom.CTabFolder;
 import org.eclipse.swt.custom.CTabItem;
 import org.eclipse.swt.custom.StyledText;
+import org.eclipse.swt.events.MouseEvent;
+import org.eclipse.swt.events.MouseListener;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.events.SelectionListener;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Menu;
+import org.eclipse.swt.widgets.MenuItem;
 
-public class DataEditorPart {
+public class DataEditorPart implements MouseListener, SelectionListener{
 	
 	private CTabFolder tabFolder;
-	private StyledText styledText;
+	private Menu contextMenu;
+	private MenuItem newItem;
+	private MenuItem cutItem;
+	private MenuItem copyItem;
+	private MenuItem pasteItem;
 	
 	@Inject
 	public DataEditorPart() {
+		
 	}
 	
 	@PostConstruct
 	public void createComposite(Composite parent) {
 		tabFolder = new CTabFolder(parent,SWT.NONE);
-		CTabItem newTab = new CTabItem(tabFolder,SWT.NONE);
+		createTab();
+	}
+	
+	public void createTab(){
+		CTabItem newTab = new CTabItem(tabFolder,SWT.CLOSE);
 		newTab.setText("New File");
-		tabFolder.setSelection(0);
-		styledText = new StyledText(tabFolder,SWT.V_SCROLL|SWT.H_SCROLL);
-		styledText.setAlwaysShowScrollBars(true);
-		styledText.setText("Testing 1 2 3");
-		newTab.setControl(styledText);
-		
+		tabFolder.setSelection(newTab);
+		StyledText textArea = new StyledText(tabFolder,SWT.V_SCROLL|SWT.H_SCROLL);
+		textArea.addMouseListener(this);
+		textArea.setAlwaysShowScrollBars(true);
+		newTab.setControl(textArea);
+	}
+	
+	public void createTab(String fileName, String[] contents){
+		CTabItem newTab = new CTabItem(tabFolder,SWT.CLOSE);
+		newTab.setText(fileName);
+		tabFolder.setSelection(newTab);
+		StyledText textArea = new StyledText(tabFolder,SWT.V_SCROLL|SWT.H_SCROLL);
+		textArea.addMouseListener(this);
+		textArea.setAlwaysShowScrollBars(true);
+		newTab.setControl(textArea);
+		for(int i = 0; i < contents.length; i++){
+			textArea.append(contents[i]);
+		}
 	}
 	
 	@Focus
 	public void onFocus() {
 	}
-	
+
+	@Override
+	public void mouseDoubleClick(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseDown(MouseEvent e) {
+		if(e.button == 3){
+			Control c = (Control)e.getSource();
+			contextMenu = new Menu(c);
+			newItem = new MenuItem(contextMenu,SWT.NONE);
+			newItem.setText("New");
+			newItem.addSelectionListener(this);
+			cutItem = new MenuItem(contextMenu,SWT.NONE);
+			cutItem.setText("Cut");
+			cutItem.addSelectionListener(this);
+			copyItem = new MenuItem(contextMenu,SWT.NONE);
+			copyItem.setText("Copy");
+			copyItem.addSelectionListener(this);
+			pasteItem = new MenuItem(contextMenu,SWT.NONE);
+			pasteItem.setText("Paste");
+			pasteItem.addSelectionListener(this);
+			c.setMenu(contextMenu);
+		}
+	}
+
+	@Override
+	public void mouseUp(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void widgetSelected(SelectionEvent e) {
+		if(e.getSource() == newItem){
+			createTab();
+		}
+		if(e.getSource() == cutItem){
+			System.out.println("Cut!");
+		}
+		if(e.getSource() == copyItem){
+			System.out.println("Copy!");
+		}
+		if(e.getSource() == pasteItem){
+			System.out.println("Paste!");
+		}
+	}
+
+	@Override
+	public void widgetDefaultSelected(SelectionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
 	
 }
