ec55c81f19fb582de8fa2bad86462c7f540484a7
Kyle Mullins <kpm9359@rit.edu>
2014-02-26 16:21:22 -0500

    Made ids for everything uniform and added several commands/handlers.

---
+++ src/LinGUIne/handlers/NewFileHandler.java
@@ -0,0 +1,12 @@
+ 
+package LinGUIne.handlers;
+
+import org.eclipse.e4.core.di.annotations.Execute;
+
+public class NewFileHandler {
+	@Execute
+	public void execute() {
+		//TODO Your code goes here
+	}
+		
+}
---
+++ src/LinGUIne/handlers/SwitchPerspectiveHandler.java
@@ -0,0 +1,12 @@
+ 
+package LinGUIne.handlers;
+
+import org.eclipse.e4.core.di.annotations.Execute;
+
+public class SwitchPerspectiveHandler {
+	@Execute
+	public void execute() {
+		//TODO Your code goes here
+	}
+		
+}
