84d2943c78cf205090b0c96e4750e8f2eb5e1d75
Kyle Mullins <kpm9359@rit.edu>
2014-03-03 17:13:22 -0500

    Implemented Switch Perspective button to toggle between the two Perspectives.

--- src/LinGUIne/handlers/SwitchPerspectiveHandler.java
+++ src/LinGUIne/handlers/SwitchPerspectiveHandler.java
@@ -2,11 +2,34 @@
 package LinGUIne.handlers;
 
 import org.eclipse.e4.core.di.annotations.Execute;
+import org.eclipse.e4.ui.model.application.MApplication;
+import org.eclipse.e4.ui.model.application.ui.advanced.MPerspective;
+import org.eclipse.e4.ui.workbench.modeling.EModelService;
+import org.eclipse.e4.ui.workbench.modeling.EPartService;
 
+/**
+ * Handler to toggle between the advanced and basic Perspectives.
+ * 
+ * @author Kyle Mullins
+ */
 public class SwitchPerspectiveHandler {
 	@Execute
-	public void execute() {
-		//TODO Your code goes here
-	}
+	public void execute(MApplication app, EPartService partService,
+			EModelService modelService) {
+		MPerspective advancedPerspective = (MPerspective)modelService.find(
+				"linguine.perspective.advancedPerspective", app);
 		
+		MPerspective basicPerspective = (MPerspective)modelService.find(
+				"linguine.perspective.basicPerspective", app);
+		
+		MPerspective activePerspective = modelService.getActivePerspective(
+				app.getSelectedElement());
+		
+		if(activePerspective == advancedPerspective){
+			partService.switchPerspective(basicPerspective);
+		}
+		else{
+			partService.switchPerspective(advancedPerspective);
+		}
+	}
 }
