b8a3fcada8a49fa828b2453761325244ef56ccec
p-dimou <peter.dimou@gmail.com>
2014-03-05 19:32:53 -0500

    Created an extension point for visualization plugins. Added the Linguine.extensions package to the list of exported runtime packages so that plugins can utilize them.

---
+++ src/LinGUIne/extensions/IVisualizationPlugin.java
@@ -0,0 +1,35 @@
+package LinGUIne.extensions;
+
+/**
+ * A Visualization Plugin consists of the provider (usually a library or 
+ * software module such as JFreeChart or BIRT) and visualizations that 
+ * the provider can produce (e.g., bar graphs, pie charts, etc.)
+ * 
+ * @author Peter Dimou
+ */
+public interface IVisualizationPlugin {
+
+	/**
+	 * Get the visualization provider's name
+	 * 
+	 * @return The visualization provider's name
+	 */
+	String getName();
+	
+	/**
+	 * Get the list of all visualizations that this provider can produce.
+	 * 
+	 * @return The list of all visualizations that this plugin provides.
+	 */
+	String[] getVisualizations();
+	
+	/**
+	 * Runs the specified visualization. The name of the visualization comes
+	 * from the provider by using the getVisualizations method.
+	 * 
+	 * @param visualizationName The name of the visualization as provided by
+	 * the method getVisualizations.
+	 */
+	void runVisualization(String visualizationName);
+	
+}
