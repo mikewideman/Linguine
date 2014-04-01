5f6f7a2dda66c2548fa95eac9a6368c3f64bc653
Kyle Mullins <kpm9359@rit.edu>
2014-04-01 16:25:21 -0400

    Initial work on ExportFileWizard.

--- src/LinGUIne/handlers/ExportHandler.java
+++ src/LinGUIne/handlers/ExportHandler.java
@@ -1,10 +1,16 @@
 package LinGUIne.handlers;
 
+import javax.inject.Inject;
 import javax.inject.Named;
 
+import org.eclipse.e4.core.contexts.ContextInjectionFactory;
 import org.eclipse.e4.core.di.annotations.Execute;
+import org.eclipse.e4.ui.model.application.MApplication;
 import org.eclipse.e4.ui.services.IServiceConstants;
+import org.eclipse.jface.wizard.WizardDialog;
 import org.eclipse.swt.widgets.Shell;
+
+import LinGUIne.wizards.ExportFileWizard;
 
 /**
  * Handles exporting Project Data to other file formats.
@@ -12,8 +18,16 @@ import org.eclipse.swt.widgets.Shell;
  * @author Kyle Mullins
  */
 public class ExportHandler {
+	@Inject
+	private MApplication application;
+	
 	@Execute
 	public void execute(@Named(IServiceConstants.ACTIVE_SHELL) Shell shell) {
+		ExportFileWizard exportWizard = new ExportFileWizard();
+		WizardDialog wizardDialog = new WizardDialog(shell, exportWizard);
 		
+		ContextInjectionFactory.inject(exportWizard, application.getContext());
+		
+		wizardDialog.open();
 	}
 }
