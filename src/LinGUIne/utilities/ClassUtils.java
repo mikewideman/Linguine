522b56f34d43e5be3a97993f3327ae62c364423b
Kyle Mullins <kpm9359@rit.edu>
2014-04-29 00:03:39 -0400

    Activator stopped getting called which was bombing Class parsing at startup. Reverted to old broken Class parsing for now.

--- src/LinGUIne/utilities/ClassUtils.java
+++ src/LinGUIne/utilities/ClassUtils.java
@@ -13,16 +13,21 @@ public class ClassUtils {
 	}
 	
 	public static String serializeClassName(Class<?> clazz){
-		return getBundleIdForClass(clazz) + "-" + clazz.getCanonicalName();
+//		return getBundleIdForClass(clazz) + "-" + clazz.getCanonicalName();
+//		return getBundleNameForClass(clazz) + "-" + clazz.getCanonicalName();
+		return clazz.getCanonicalName();
 	}
 	
 	public static Class<?> deserializeClassName(String className)
 			throws ClassNotFoundException{
+		//TODO: Figure out why BundleContext isn't getting set. Fuckin' shit up
+//		long bundleId = Long.parseLong(className.split("-")[0]);
+//		String bundleLoc = className.split("-")[0];
+//		className = className.split("-")[1];
 		
-		long bundleId = Long.parseLong(className.split("-")[0]);
-		className = className.split("-")[1];
-		
-		return loadClassFromBundle(bundleId, className);
+//		return loadClassFromBundle(bundleId, className);
+//		return loadClassFromBundle(bundleLoc, className);
+		return Class.forName(className);
 	}
 	
 	public static Class<?> loadClassFromBundle(long bundleId, String className) 
@@ -33,7 +38,23 @@ public class ClassUtils {
 		return bundle.loadClass(className);
 	}
 	
+	public static Class<?> loadClassFromBundle(String bundleName,
+			String className) throws ClassNotFoundException{
+		
+		for(Bundle bundle: context.getBundles()){
+			if(bundle.getSymbolicName().equals(bundleName)){
+				return bundle.loadClass(className);
+			}
+		}
+		
+		return null;
+	}
+	
 	public static long getBundleIdForClass(Class<?> clazz){
 		return FrameworkUtil.getBundle(clazz).getBundleId();
+	}
+	
+	public static String getBundleNameForClass(Class<?> clazz){
+		return FrameworkUtil.getBundle(clazz).getSymbolicName();
 	}
 }
